327b86f53ba4761e5631c6d8dbfaf37c50aab000
Geoffrey Hayes <hayesgm@gmail.com>
2024-01-10 22:20:23 -0800

    start to make the network smarter

--- learn.jl
+++ learn.jl
@@ -5,65 +5,110 @@ using Base.MathConstants
 using PrettyTables
 using Folds
 
-function initialize_network(input_sz, output_sz, hidden_layers)
-
-  network_shape = [input_sz; hidden_layers; output_sz]
-  
-  map(rand(x,y) for (x, y) ∈ zip(network_shape[1:end-1], network_shape[2:end]))
-end
-
+softmax(x)=e^x
+softmax_derivative(x) = x * ( 1 - x ) # TODO: Fix this up
 sigmoid(x) = 1 / (1 + e^-x)
 sigmoid_derivative(x) = x * ( 1 - x )
 squared_difference(target, actual) = 1//2 * sum( ( target .- actual ) .^2 )
 squared_difference_derivative(target, actual) = -( target .- actual )
 
 activation_fns = Dict(
-  "sigmoid" => (sigmoid, sigmoid_derivative)
+  "sigmoid" => (sigmoid, sigmoid_derivative),
+  "softmax" => (softmax, softmax_derivative)
 )
+
+function fetch!(dict, key, err="key not found")
+  if haskey(dict, key)
+    dict[key]
+  else
+    error(err)
+  end
+end
+
+# TODO: Not always rand weights
+function initialize_layer((layers, prev_sz), layer_config)
+  activation_fn = layer_config[1]
+
+  (layer_sz, weights, scaled) = if activation_fn == "softmax"
+    (prev_sz, nothing, true)
+  else
+    sz = layer_config[2]
+
+    (sz, rand(prev_sz, sz), false)
+  end
+
+  (activation, activation_derivative) = fetch!(activation_fns, activation_fn, "unknown activation function: $activation_fn")
+
+  layer = (config=layer_config, weights=weights, activation=activation, activation_derivative=activation_derivative, scaled=scaled)
+
+  ([layers; [layer]], layer_sz)
+end
+
+function initialize_network(input_sz, layer_configs)
+  (layers, _) = Folds.reduce(initialize_layer, layer_configs, init=([], input_sz))
+
+  layers
+end
 
 cost_fns = Dict(
   "squared-difference" => (squared_difference, squared_difference_derivative)
 )
 
-function show_network(neural_network)
-  max_length = maximum(length.(neural_network))
-  padded_vecs = [vcat(vec, fill(NaN, max_length - length(vec))) for vec in reverse(neural_network)]
+function show_network(nn)
+  max_length = maximum(length.(nn))
+  padded_vecs = [vcat(vec, fill(NaN, max_length - length(vec))) for vec in reverse(nn)]
   mat = hcat(padded_vecs...)'
   pretty_table(mat, header=repeat(["Column"], max_length))
 end
 
-function network(;network_shape, embedding, training, debug=false, show_fn=show_fn, activation_fn="sigmoid", cost_fn="squared-difference", ϵ=0.01)
-  neural_network = initialize_network(network_shape)
-  (activation, activation_derivative) = get(activation_fns, activation_fn, () -> error("unknown activation function: $(activation_fn)"))
-  (cost, cost_derivative) = get(cost_fns, cost_fn, () -> error("unknown cost function: $(cost_fn)"))
+function apply_layer(layer, prev_values)
+  values = if layer.weights !== nothing
+    [layer.activation.(dot(prev_values, w)) for w ∈ eachcol(layer.weights)]
+  else
+    layer.activation.(prev_values)
+  end
+
+  if layer.scaled
+    values / sum(values)
+  else
+    values
+  end
+end
+
+function build_nn(;network_layers, embedding, training, show_fn=show_fn, activation_fn="sigmoid", cost_fn="squared-difference", ϵ=0.01)
+  apply_embedding(k) = embedding[k]
+
+  input_sz = length(apply_embedding(rand(training)[begin]))
+  nn = initialize_network(input_sz, network_layers)
+  (activation, activation_derivative) = fetch!(activation_fns, activation_fn, "unknown activation function: $activation_fn")
+  (cost, cost_derivative) = fetch!(cost_fns, cost_fn, "unknown cost function: $cost_fn")
 
   grammar = keys(embedding)
 
-  apply_embedding(k) = embedding[k]
   inverse_embedding = Dict([(v, k) for (k, v) ∈ pairs(embedding)])
 
   function show_output(output)
     max_length = maximum(length.(output))
     padded_vecs = [vcat(vec, fill(NaN, max_length - length(vec))) for vec in reverse(output)]
     mat = hcat(padded_vecs...)'
-    pretty_table(mat, header=repeat(["Column"], max_length))
+    pretty_table(mat, header=repeat([""], max_length))
   end
 
-  function train(neural_network; case=nothing)
+  function train(nn; case=nothing, debug=false)
     (input, target) = if case == nothing
       rand(training)
     else
-      (case, get(training, case, () -> error("unknown training case: $case")))
+      (case, fetch!(training, case, "unknown training case: $case"))
     end
 
     embedded_input = apply_embedding(input)
 
-    function calculate_next_layer_with_hidden(layers, weights)
-      [layers; [[activation.(dot(layers[end], w)) for w ∈ eachcol(weights)]]]
+    function calculate_next_layer_with_hidden(values, layer)
+      [values; [apply_layer(layer, values[end])]]
     end
-     
-    function calculate_value_with_hidden(layer0)
-      Folds.reduce(calculate_next_layer_with_hidden, neural_network; init=[layer0])
+
+    function calculate_value_with_hidden(input)
+      Folds.reduce(calculate_next_layer_with_hidden, nn; init=[input])
     end
 
     output_with_hidden = calculate_value_with_hidden(embedded_input)
@@ -77,72 +122,64 @@ function network(;network_shape, embedding, training, debug=false, show_fn=show_
 
     # First, let's calculate our overall error
     error = cost(target, output_with_hidden[end])
+    ∂E∂yend=cost_derivative(target, output_with_hidden[end])
 
-    function backpropagate(acc, (j, (yi, yj)))
+    function backpropagate(acc, (j, (layer, (yi, yj))))
       # This reduction works backwards through the outputs of the
       # neural net. Each step computes the weight changes for that
       # layer and then propagates backwards. The result is several
       # named vectors (∂E∂yj=...,∂E∂zj=...,∆weightj=...,weightj=...)
       
       # Note: y refers to current layer, i to previous layer
-      # Note: `i` could technically be the input layer
 
-      # We are calculating ∂E/∂yj which is defined as -(tj - yj)
-      # TODO: I believe this is only for squared-difference cost function
-      # TODO: Maybe zip?
-      weightsj = neural_network[j]
-
-      if false
-        show("acc=$acc")
-        show("j=$j")
-        show("yj=$yj")
-        show("yi=$yi")
-        show("weightsj=$weightsj")
+      if debug
+        display("acc=$acc")
+        display("j=$j")
+        display("yj=$yj")
+        display("yi=$yi")
+        display("layer=$layer")
       end
-      # We need to peek at the next row of output to get `yi`
 
-      # TODO: Is this accurate only for the first layer?
-      # Again, maybe we can zip to combine data?
+      # By definition, ∂E∂yj = ∂E∂yi from the previous layer
+      ∂E∂yj = acc[end].∂E∂yi
+      ∂E∂zj = activation_derivative.(yj) .* ∂E∂yj
 
-      ∂E∂yj = 
-        if size(acc) == (0,)
-          # Base case, output layer
-          # TODO: Toy with this assumption
-          cost_derivative(target, yj)
-        else
-          weightsk = neural_network[j+1]
+      # Calculate ∂E∂yi, which will be `∂E∂yj` for the next iteration
+      (∂E∂yi, layer_adj) = if layer.config[1] == "softmax" 
+        # I need to mull this, but I believe since there are no weights
+        # we just pass this through directly?
+        (∂E∂zj, layer)
+      else
+        # This is the "caching" part of the back propagating algorithm
+        # that uses previously calculated values
+        ∂E∂yi = [ sum(neuron .* ∂E∂zj) for neuron ∈ eachrow(layer.weights) ]
+        ∆weightj=reshape([-ϵ*i*j for i in ∂E∂zj for j in yi], size(layer.weights))
+        weightj_adj=layer.weights .+ ∆weightj
+        layer_adj=(; layer..., weights=weightj_adj)
 
-          # This is the "caching" part of the back propagating algorithm
-          # that uses previously calculated values
-          [ sum(neuron .* acc[end].∂E∂zj) for neuron ∈ eachrow(weightsk) ]
-        end
+        (∂E∂yi, layer_adj)
+      end
 
-      ∂E∂zj = activation_derivative.(yj) .* ∂E∂yj      
-
-      ∆weightj=reshape([-ϵ*i*j for i in ∂E∂zj for j in yi], size(weightsj))
-
-      weightj_adj=weightsj .+ ∆weightj
-
-      [acc; (∂E∂yj=∂E∂yj,∂E∂zj=∂E∂zj,∆weightj=∆weightj,weightj=weightj_adj)]
+      [acc; (∂E∂yi=∂E∂yi,∂E∂zj=∂E∂zj,layer_adj=layer_adj)]
     end
 
-    output_pairs = zip(output_with_hidden[1:end-1], output_with_hidden[2:end])
+    output_pairs = zip(nn, zip(output_with_hidden[1:end-1], output_with_hidden[2:end]))
 
-    res = Folds.reduce(backpropagate, Iterators.reverse(enumerate(output_pairs)); init=[])
+    res = Folds.reduce(backpropagate, Iterators.reverse(enumerate(output_pairs)); init=[(∂E∂yi=∂E∂yend,)])
 
-    map(res -> res.weightj, reverse(res))
+    map(res -> res.layer_adj, reverse(res[2:end]))
   end
 
-  function infer(neural_network, input)
+  function infer(nn, input)
     embedded_input = apply_embedding.(input)
 
     # Just do these one at a time until we parallelize
-    function calculate_next_layer(layer, weights)
-      [activation.(dot(layer, w)) for w ∈ eachcol(weights)]
+    function calculate_next_layer(values, layer)
+      apply_layer(layer, values)
     end
 
-    function calculate_value(layer0)
-      Folds.reduce(calculate_next_layer, neural_network; init=layer0)
+    function calculate_value(input)
+      Folds.reduce(calculate_next_layer, nn; init=input)
     end
 
     output = [calculate_value(i) for i ∈ embedded_input]
@@ -150,7 +187,7 @@ function network(;network_shape, embedding, training, debug=false, show_fn=show_
     show_fn(output)
   end
 
-  (neural_network, train, infer)
+  (nn, train, infer)
 end
 
 # This could probably be made using an identity matrix and an index paramter
@@ -190,7 +227,17 @@ function show_sixel(output)
   sixel_encode(output_rgb)
 end
 
-(nn, train, infer) = network(network_shape=(3,4), embedding=embedding, training=training, show_fn=show_sixel, ϵ=0.05)
+(nn, train, infer) = build_nn(
+  network_layers=[
+    ("sigmoid", 4),
+    ("sigmoid", 3),
+    ("softmax", 3)
+  ],
+  embedding=embedding,
+  training=training,
+  show_fn=show_sixel,
+  ϵ=0.05
+)
 
 for i ∈ 1:10
   for j ∈ 1:20000
@@ -201,29 +248,3 @@ for i ∈ 1:10
 end
 
 show(nn)
-
-##
-
-# [show_output(o) for o ∈ output_with_hidden]
-# input
-# sixel_encode(output_rgb)
-# target
-# actual
-# error
-
-# I don't think we need this
-# Then to the initial layer
-# ∂E∂yh = [ sum(neuron .* ∂E∂zi) for neuron ∈ eachrow(neural_network[1]) ]
-# actualh = output_with_hidden[1][1]
-# ∂E∂zh = actualh .* ( ones(size(actualh)) - actualh ) .* ∂E∂yh
-
-# neural_network_0 = neural_network
-# neural_network_1 = [weighti, weightj]
-# neural_network_2 = [weighti, weightj]
-
-# julia> error
-# 0.3832055164790629
-
-# Error went up with new weights
-# error = 1//2 * sum( ( target .- actual ) .^2 )
-# 0.43654530839709255
