7ed4096d3abacfcdef400f704b2b56727a99fa47
Geoffrey Hayes <hayesgm@gmail.com>
2024-01-14 12:29:03 -0800

    Run formatter

--- learn.jl
+++ learn.jl
@@ -6,17 +6,20 @@ using PrettyTables
 using Folds
 using Statistics
 
-softmax(x)=e^x
-softmax_derivative(x) = x * ( 1 - x ) # TODO: Fix this up
+softmax(x) = e^x
+softmax_derivative(x) = x * (1 - x) # TODO: Fix this up
+relu(x) = x > 0 ? x : 0
+relu_derivative(x) = x > 0 ? 1 : 0
 sigmoid(x) = 1 / (1 + e^-x)
-sigmoid_derivative(x) = x * ( 1 - x )
-squared_difference(target, actual) = 1//2 * sum( ( target .- actual ) .^2 )
-squared_difference_derivative(target, actual) = -( target .- actual )
-cross_entropy(target, actual) = -sum( target .* log.(actual) )
+sigmoid_derivative(x) = x * (1 - x)
+squared_difference(target, actual) = 1 // 2 * sum((target .- actual) .^ 2)
+squared_difference_derivative(target, actual) = -(target .- actual)
+cross_entropy(target, actual) = -sum(target .* log.(actual))
 cross_entropy_derivative(target, actual) = actual .- target
 
 activation_fns = Dict(
   "sigmoid" => (sigmoid, sigmoid_derivative),
+  "relu" => (relu, relu_derivative),
   "softmax" => (softmax, softmax_derivative)
 )
 
@@ -33,24 +36,37 @@ function fetch!(dict, key, err="key not found")
   end
 end
 
+function get_opt(opts, key, default)
+  if opts !== nothing && key in fieldnames(typeof(opts))
+    opts[key]
+  else
+    default
+  end
+end
+
 # TODO: Not always rand weights
-function initialize_layer((layers, prev_sz), layer_config)
+function initialize_layer((layers, layer_num, prev_sz, network_config), layer_config)
   (layer_sz, weights, scaled) = if layer_config.type == "softmax"
     (prev_sz, nothing, true)
   else
     sz = layer_config.nodes
 
-    (sz, rand(prev_sz, sz), false)
+    if false && get_opt(network_config, :deterministic, false)
+      matrix = [5 6 7; 8 9 10]
+      (sz, matrix, false)
+    else
+      (sz, rand(prev_sz, sz), false)
+    end
   end
 
   activation_fn = layer_config.type # currently 1:1
   (activation, activation_derivative) = fetch!(activation_fns, activation_fn, "unknown activation function: $activation_fn")
 
   apply_weight_constraints = if :constraints in fieldnames(typeof(layer_config))
-    function(∂E∂wijs)
-      for ((x1,y1),(x2,y2)) in layer_config.constraints
-        ∂E∂wijs[x1,y1] = ∂E∂wijs[x2,y2] =
-          ( ( ∂E∂wijs[x1,y1] + ∂E∂wijs[x2,y2] ) / 2 )
+    function (∂E∂wijs)
+      for ((x1, y1), (x2, y2)) in layer_config.constraints
+        ∂E∂wijs[x1, y1] = ∂E∂wijs[x2, y2] =
+          ((∂E∂wijs[x1, y1] + ∂E∂wijs[x2, y2]) / 2)
       end
       ∂E∂wijs
     end
@@ -67,11 +83,11 @@ function initialize_layer((layers, prev_sz), layer_config)
     apply_weight_constraints=apply_weight_constraints
   )
 
-  ([layers; [layer]], layer_sz)
+  ([layers; [layer]], layer_num + 1, layer_sz, network_config)
 end
 
-function initialize_network(input_sz, layer_configs)
-  (layers, _) = Folds.reduce(initialize_layer, layer_configs, init=([], input_sz))
+function initialize_network(input_sz, layer_configs; network_config=nothing)
+  (layers, _) = Folds.reduce(initialize_layer, layer_configs, init=([], 1, input_sz, network_config))
 
   layers
 end
@@ -97,11 +113,11 @@ function apply_layer(layer, prev_values)
   end
 end
 
-function build_nn(;network_layers, embedding, training, show_fn=show_fn, cost_fn="squared-difference", ϵ=0.01)
+function build_nn(; network_layers, embedding, training, show_fn=show_fn, cost_fn="squared-difference", ϵ=0.01, network_config=nothing)
   apply_embedding(k) = embedding[k]
 
   input_sz = length(apply_embedding(rand(training)[begin]))
-  nn = initialize_network(input_sz, network_layers)
+  nn = initialize_network(input_sz, network_layers, network_config=network_config)
   (cost, cost_derivative) = fetch!(cost_fns, cost_fn, "unknown cost function: $cost_fn")
 
   grammar = keys(embedding)
@@ -116,16 +132,16 @@ function build_nn(;network_layers, embedding, training, show_fn=show_fn, cost_fn
   end
 
   function get_case(case)
-      fetch!(training, case, "unknown training case: $case")
-    end
+    fetch!(training, case, "unknown training case: $case")
+  end
 
   function train(nn; case=nothing, batch=nothing, debug=false)
     inputs = if batch == nothing
       if case == nothing
-        [rand(training), rand(training), rand(training), rand(training), rand(training), rand(training)]
+        [rand(training), rand(training)]
       else
-       [(case, get_case(case))]
-     end
+        [(case, get_case(case))]
+      end
     else
       map(case -> (case, get_case(case)), batch)
     end
@@ -153,7 +169,7 @@ function build_nn(;network_layers, embedding, training, show_fn=show_fn, cost_fn
       # First, let's calculate our overall error
       actual = output_with_hidden[end]
       error = cost(target, actual)
-      ∂E∂yifinal=cost_derivative(target, actual)
+      ∂E∂yifinal = cost_derivative(target, actual)
       ∂E∂zifinal = nn[end].activation_derivative.(actual) .* ∂E∂yifinal
 
       function backpropagate((∂E∂yis, ∂E∂zis, ∂E∂wijs), (layer, yprev))
@@ -161,7 +177,7 @@ function build_nn(;network_layers, embedding, training, show_fn=show_fn, cost_fn
         # neural net. Each step computes the weight changes for that
         # layer and then propagates backwards. The result is several
         # named vectors (∂E∂yj=...,∂E∂zj=...,∆weightj=...,weightj=...)
-        
+
         # Note: i refers to current layer, y to next layer up
         # Note: Due to backpropagation, y was calculated before i
 
@@ -173,17 +189,17 @@ function build_nn(;network_layers, embedding, training, show_fn=show_fn, cost_fn
           display("yprev=$yprev")
         end
 
-        ∂E∂zj=∂E∂zis[end]
+        ∂E∂zj = ∂E∂zis[end]
 
         ∂E∂wij = if layer.config.type == "softmax"
           nothing
         else
-          reshape([zj*y for zj in ∂E∂zj for y in yprev], size(layer.weights))
+          reshape([zj * y for zj in ∂E∂zj for y in yprev], size(layer.weights))
         end
 
         # Calculate ∂E∂yi, which will be `∂E∂yj` for the next iteration
         # 4 in length
-        ∂E∂yi = if layer.config.type == "softmax" 
+        ∂E∂yi = if layer.config.type == "softmax"
           # I need to mull this, but I believe since there are no weights
           # we just pass this through directly?
           # Note: this is really ∂zj∂yi
@@ -191,7 +207,7 @@ function build_nn(;network_layers, embedding, training, show_fn=show_fn, cost_fn
         else
           # This is the "caching" part of the back propagating algorithm
           # that uses previously calculated values
-          [ sum(neuron .* ∂E∂zj) for neuron ∈ eachrow(layer.weights) ]
+          [sum(neuron .* ∂E∂zj) for neuron ∈ eachrow(layer.weights)]
         end
 
         # display("∂E∂yi=$∂E∂yi")
@@ -205,24 +221,24 @@ function build_nn(;network_layers, embedding, training, show_fn=show_fn, cost_fn
       layer_prev_values = zip(nn, output_with_hidden[1:end-1])
 
       (∂E∂yis_rev, ∂E∂zis_rev, ∂E∂wijs_rev) = Folds.reduce(backpropagate, Iterators.reverse(layer_prev_values); init=([∂E∂yifinal], [∂E∂zifinal], [nothing]))
-      ∂E∂yis=Iterators.reverse(∂E∂yis_rev)
-      ∂E∂zis=Iterators.reverse(∂E∂zis_rev)
-      ∂E∂wijs=Iterators.reverse(∂E∂wijs_rev)
+      ∂E∂yis = Iterators.reverse(∂E∂yis_rev)
+      ∂E∂zis = Iterators.reverse(∂E∂zis_rev)
+      ∂E∂wijs = Iterators.reverse(∂E∂wijs_rev)
 
       [∂E∂wijs...]
     end
 
     gradients = map(derive_gradients, inputs)
 
-    gradient_layers = [ [gradient[i] for gradient in gradients] for i in 1:length(gradients[1]) ]
+    gradient_layers = [[gradient[i] for gradient in gradients] for i in 1:length(gradients[1])]
 
     function reweight((layer, ∂E∂wijs))
       # display("∂E∂wijs=$∂E∂wijs")
       if layer.weights == nothing
         layer
       else
-        ∆weight=-ϵ .* layer.apply_weight_constraints(mean(∂E∂wijs))
-        weights_adj=layer.weights .+ ∆weight
+        ∆weight = -ϵ .* layer.apply_weight_constraints(mean(∂E∂wijs))
+        weights_adj = layer.weights .+ ∆weight
 
         (; layer..., weights=weights_adj)
       end
